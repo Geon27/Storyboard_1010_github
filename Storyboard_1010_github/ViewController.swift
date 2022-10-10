0c2f3c522040675c098ff302b6f962943e046665
Geon_27 <yukgeon98527@gmail.com>
2022-10-10 20:36:40 +0900

    Initial Commit

---
+++ Storyboard_1010_github/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Storyboard_1010_github
+//
+//  Created by 육건 on 2022/10/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
